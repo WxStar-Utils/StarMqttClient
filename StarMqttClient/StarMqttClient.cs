9f1cbbb67c9f828b6a104000c9f7643153389b6b
mewtek <april.p@mistwx.com>
2025-05-27 23:26:36 -0700

    Add MQTT client initialization

---
+++ StarMqttClient/StarMqttClient.cs
@@ -0,0 +1,51 @@
+﻿using MQTTnet;
+using MQTTnet.Client;
+
+namespace StarMqttClient;
+
+public partial class StarMqttClient
+{
+    private readonly IMqttClient _mqttClient;
+    private readonly string _host;
+    private readonly int _port;
+    private readonly string _username;
+    private readonly string _password;
+    public string ClientId { get; }
+
+    public StarMqttClient(string host, int port, string username, string password, string clientId)
+    {
+        _host = host;
+        _port = port;
+        _username = username;
+        _password = password;
+        ClientId = clientId;
+        
+        // Initialize the MQTTNet client object
+        var factory = new MqttFactory();
+        _mqttClient = factory.CreateMqttClient();
+    }
+
+    /// <summary>
+    /// Connects the created MQTT client to a broker.
+    /// </summary>
+    public async Task Connect()
+    {
+        var clientOptions = new MqttClientOptionsBuilder()
+            .WithClientId(ClientId)
+            .WithTcpServer(_host, _port)
+            .WithCredentials(_username, _password)
+            .WithCleanSession()
+            .Build();
+
+        await _mqttClient.ConnectAsync(clientOptions);
+    }
+
+    /// <summary>
+    /// Disconnects the MQTT client socket.
+    /// </summary>
+    public async Task Disconnect()
+    {
+        await _mqttClient.DisconnectAsync();
+    }
+    
+}
